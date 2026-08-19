e6cc33e1013cec7a8921056a37fe5888b6c1adb2
Noddy0070 <utkarshsaxena@rediffmail.com>
2026-08-19 11:27:48 +0530

    added tags

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -236,7 +236,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
       {/* Google Analytics */}
       <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
+      {/* <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -244,9 +244,22 @@ function MyApp({ Component, pageProps }: AppProps) {
           gtag('config', '${GA_TRACKING_ID}', {
             page_path: window.location.pathname,
           });
+        `} */}
+      {/* </Script> */}
+        <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MWD5JLNK');
         `}
       </Script>
 
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MWD5JLNK"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
       <ThemeProvider>
         <StaticAttributesProvider>
         <SearchProvider>
